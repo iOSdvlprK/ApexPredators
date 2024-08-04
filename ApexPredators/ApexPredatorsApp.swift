8b88e5425b9b613e31fc82ed3b58ffecfe5c75eb
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-08-04 22:04:05 +0900

    Initial commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by joe on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
