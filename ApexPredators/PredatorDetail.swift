39c6895ceeed93756deb79de63b9d2feb2e4ea16
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-08-10 23:50:24 +0900

    more about PredatorDetail

--- ApexPredators/PredatorDetail.swift
+++ ApexPredators/PredatorDetail.swift
@@ -29,15 +29,47 @@ struct PredatorDetail: View {
                         .offset(y: 20)
                 }
                 
-                // dino name
-                
-                // current location
-                
-                // appears in
-                
-                // movie moments
-                
-                // link to webpage
+                VStack(alignment: .leading) {
+                    // dino name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    // current location
+                    
+                    // appears in
+                    Text("Appears In:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("• " + movie)
+                            .font(.subheadline)
+                    }
+                    
+                    // movie moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    
+                    // link to webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
             }
             .ignoresSafeArea()
         }
