503a5bcbc88726a8965185722d93810f807b5e62
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-08-14 23:53:30 +0900

    implement map view with map position & styles

--- ApexPredators/PredatorDetail.swift
+++ ApexPredators/PredatorDetail.swift
@@ -45,9 +45,7 @@ struct PredatorDetail: View {
                     
                     // current location
                     NavigationLink {
-                        Image(predator.image)
-                            .resizable()
-                            .scaledToFit()
+                        PredatorMap(position: .camera(MapCamera(centerCoordinate: predator.location, distance: 1000, heading: 250, pitch: 80)))
                     } label: {
                         Map(position: $position) {
                             Annotation(predator.name, coordinate: predator.location) {
